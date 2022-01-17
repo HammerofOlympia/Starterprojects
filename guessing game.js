1144e628e1e1616b1fd929456d794419a7031013
Daniel Wolfe <88575455+HammerofOlympia@users.noreply.github.com>
2022-01-17 14:39:51 +0000

    Add files via upload

---
+++ guessing game.js
@@ -0,0 +1,10 @@
+ const form = document.querySelector("#pick");
+form.addEventListener("submit", function (e) {
+    
+
+   console.log("submit") ;
+   console.log("submit") ;
+   console.log("submit") ;
+   console.log("submit") ;
+   console.log("submit") ;
+});
