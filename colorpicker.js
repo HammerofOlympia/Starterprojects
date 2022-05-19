23210ea89e00f88c741bced2c35ecb57cb32ca5e
Daniel Wolfe <Dwolfe1985@hotmail.com>
2022-05-19 16:30:35 +0100

    some minor positional changes

---
+++ colorpicker.js
@@ -0,0 +1,32 @@
+const button=document.querySelector("button");
+const h1 =document.querySelector ("h1");
+
+button.addEventListener("click",()=>{
+  const newColor=randomColor();
+  document.body.style.backgroundColor=newColor;
+  h1.innerText=newColor;
+})
+
+const randomColor=()=>{
+  const r=Math.floor(Math.random()*255);
+  const g=Math.floor(Math.random()*255);
+  const b=Math.floor(Math.random()*255);
+  if (r+g+b <200){
+    h1.style.color= "white";
+ }else{
+   h1.style.color="black";
+
+ 
+  
+}  
+return `rgb(${r},${g},${b})`;
+ 
+ };
+
+
+  
+  
+
+
+
+
