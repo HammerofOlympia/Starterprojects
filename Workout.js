f370602acd4c11320eea1242be562e860acb633c
Daniel Wolfe <Dwolfe1985@hotmail.com>
2022-06-11 21:15:21 +0100

    trying to get all enements to move

--- Workout.js
+++ Workout.js
@@ -1,1 +1,10 @@
 // on mouseclick event to translate all buttons off the page and possibly use a opacity fade effect to bring in the new one
+const buttons=document.querySelectorAll(".buttons");
+
+
+    buttons.forEach(span => {
+        buttons.addEventListener("click",(ev)=>{
+        buttons.classList.toggle("pageTransition");
+    }) 
+        
+});   
